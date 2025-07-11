c1fe6e3242dc19e642c1c050603946aeeacd20e0
vitaly <optinsoft@gmail.com>
2025-07-11 12:47:31 +0500

    manifest top empty line has been removed; make_release created the release folder

--- make_release.ps1
+++ make_release.ps1
@@ -5,31 +5,37 @@ if(!$version) {
     exit 1
 }
 
+$release_folder = "./release"
+
+if (-not (Test-Path -Path $release_folder -PathType Container)) {
+    New-Item -Path $release_folder -ItemType Directory
+}
+
 $compress_chrome_manifest2 = @{
     Path = "chrome-manifest2/*"
     CompressionLevel = "Fastest"
-    DestinationPath = "./release/ProxyAuthExtension-chrome-manifest2-v$version.zip"
+    DestinationPath = "$release_folder/ProxyAuthExtension-chrome-manifest2-v$version.zip"
 }
 Compress-Archive @compress_chrome_manifest2
 
 $compress_chrome_manifest3 = @{
     Path = "chrome-manifest3/*"
     CompressionLevel = "Fastest"
-    DestinationPath = "./release/ProxyAuthExtension-chrome-manifest3-v$version.zip"
+    DestinationPath = "$release_folder/ProxyAuthExtension-chrome-manifest3-v$version.zip"
 }
 Compress-Archive @compress_chrome_manifest3
 
 $compress_firefox_manifest2 = @{
     Path = "firefox-manifest2/*"
     CompressionLevel = "Fastest"
-    DestinationPath = "./release/ProxyAuthExtension-firefox-manifest2-v$version.zip"
+    DestinationPath = "$release_folder/ProxyAuthExtension-firefox-manifest2-v$version.zip"
 }
 Compress-Archive @compress_firefox_manifest2
 
 $compress_firefox_manifest3 = @{
     Path = "firefox-manifest3/*"
     CompressionLevel = "Fastest"
-    DestinationPath = "./release/ProxyAuthExtension-firefox-manifest3-v$version.zip"
+    DestinationPath = "$release_folder/ProxyAuthExtension-firefox-manifest3-v$version.zip"
 }
 Compress-Archive @compress_firefox_manifest3
 
